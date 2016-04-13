144b6dd5bf1b6e82a17c0d36de2fffbe76b0b88e
Howard.Zuo <leftstick@qq.com>
2016-04-13 09:44:08 +0800

    update demo with new dependencies

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,6 @@
-'use strict';
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts"/>
 
 import 'es6-shim';
-import 'reflect-metadata';
 import 'angular2/bundles/angular2-polyfills';
 import {bootstrap} from 'angular2/platform/browser';
 import {MainApp} from './map.app';
