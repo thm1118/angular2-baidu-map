53052dfc9fff3345af1bcef90d0afce1899b7bc2
Howard.Zuo <leftstick@qq.com>
2016-07-01 15:09:17 +0800

    update demo

---
+++ 10d862fb59c96e1029ac.index.bundle.js
@@ -0,0 +1,18 @@
+webpackJsonp([0,3],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	__webpack_require__.e/* nsure */(1, function (require) {
+	    __webpack_require__(1).use();
+	    __webpack_require__(5).Splash.enable('circular');
+	});
+	__webpack_require__.e/* nsure */(2, function (require) {
+	    __webpack_require__(6).use();
+	    var App = __webpack_require__(35).default;
+	    (new App()).run();
+	});
+
+
+/***/ }
+]);
