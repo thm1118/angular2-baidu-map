274a9f3803166c6f2f2ff610d68133ce89c91583
Howard.Zuo <leftstick@qq.com>
2017-11-04 19:34:59 +0800

    Finish: update doc

--- demo/components/home/version.component.ts
+++ demo/components/home/version.component.ts
@@ -5,13 +5,12 @@ import { Component } from '@angular/core'
   template: `
     <h1>Version</h1>
     <p>
-    There are 3 major versions available, and each of them is not backwards compatible. 
-    Which means you should not writing codes at version 2's instruction, but with version 3 installed. 
+    There are 2 major versions available, and each of them is not backwards compatible. 
+    Which means you should not writing codes at version 4's instruction, but with version 3 installed. 
     Below are documentation references for previous versions:
     </p>
     <br/>
     <ul>
-        <li><a href="https://github.com/leftstick/angular2-baidu-map/tree/3.x" target="_blank">version 2.x</a></li>
         <li><a href="https://github.com/leftstick/angular2-baidu-map/tree/3.x" target="_blank">version 3.x</a></li>
     </ul>
     `
